3742cadade95df13d59444a0ac6b129b05c51222
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-10-31 21:07:00 +0800

    feat: include newly added domain in cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 //Middleware
 app.use(cors({
-    origin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173", "https://dev-notesve-api.onrender.com"]
 }))
 app.use(express.json()) //req.body
 
